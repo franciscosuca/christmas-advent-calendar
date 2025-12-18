17c0ee68b696c6a1fe0be1b0889e933cefb840ca
Francisco <francisco.susana@gea.com>
2025-12-18 22:50:35 +0100

    chore: deactivate simulated date.

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@
 
 // Set this to true to simulate a specific date (for testing)
 // Set to false to use the user's real system date (for production)
-export const USE_SIMULATED_DATE = true;
+export const USE_SIMULATED_DATE = false;
 // If USE_SIMULATED_DATE is true, the app behaves as if it is this day:
 // Format: YYYY-MM-DD
 export const SIMULATED_DATE = '2024-12-19';
