ae62dc5a0e0fb6970e8d2ed58663304b573dee40
Francisco <francisco.susana@gea.com>
2025-12-18 22:46:52 +0100

    chore: test application with only 1 builder.

--- constants.ts
+++ constants.ts
@@ -3,10 +3,10 @@
 
 // Set this to true to simulate a specific date (for testing)
 // Set to false to use the user's real system date (for production)
-export const USE_SIMULATED_DATE = false;
+export const USE_SIMULATED_DATE = true;
 // If USE_SIMULATED_DATE is true, the app behaves as if it is this day:
 // Format: YYYY-MM-DD
-export const SIMULATED_DATE = '2024-12-24';
+export const SIMULATED_DATE = '2024-12-19';
 
 export const APP_URL = window.location.origin + window.location.pathname;
 
